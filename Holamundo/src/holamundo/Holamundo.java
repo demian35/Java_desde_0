b7e9fd758b808cc6fed525aa7960ebdc1a2ac49b
Demian <53890358+demian35@users.noreply.github.com>
2024-07-04 16:00:48 -0600

    Se agrego la primer practica con java el hola mundo

---
+++ Holamundo/src/holamundo/Holamundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author josed
+ */
+
+/* clase para imprimir un hola mundo
+y otra cadena*/
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");//hola mundo
+        System.out.println("Al fin corrio esta cosa");//otra cadena
+    }
+    
+}
