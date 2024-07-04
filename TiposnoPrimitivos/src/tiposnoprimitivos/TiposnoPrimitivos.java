60d7dec447cbc85a70c83e3db4b2661e79c5ff06
Demian <53890358+demian35@users.noreply.github.com>
2024-07-04 17:11:11 -0600

    Uso de tipos de datos no primitivos con java

---
+++ TiposnoPrimitivos/src/tiposnoprimitivos/TiposnoPrimitivos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposnoprimitivos;
+
+/**
+ *
+ * @author josed
+ */
+
+/* Clase en donde se muestran los tipos no primitivos de java */
+public class TiposnoPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //tipo Integer instancia de un Integer
+        Integer numero=null;
+        System.out.println("Numero del tipo Integer: " + numero);
+        
+        
+        //Tipo String con instancia
+        String cadena="Hola mundo";
+        System.out.println("Instacia de un string: " + cadena);
+    }
+    
+}
