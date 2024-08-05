f85f4eb3bbe6ca9e49b7c44225525071e2f4b521
Demian <53890358+demian35@users.noreply.github.com>
2024-08-05 14:55:30 -0600

    Usando algunos metodos de la clase math

---
+++ EjemplosMath/src/ejemplosmath/EjemplosMath.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplosmath;
+
+/**
+ *
+ * @author josed
+ */
+
+/** clase donde se usan algunos metodos de la clase math */
+public class EjemplosMath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double numero1=3;
+        double numero2=2;
+        
+        double numero3=4;
+        
+        double mayorQue=Math.max(numero1, numero2);
+        double raizcuadrada=Math.sqrt(numero3);
+        double exponenciacion=Math.pow(numero3,numero2);
+        
+        System.out.println(exponenciacion);
+        
+        
+    }
+    
+}
